1313eee9b544cd385bfb7de9a64160be0727e97c
ChivenZhang <1759774248@qq.com>
2024-10-19 16:15:23 +0800

    add comment

--- OpenARG/OpenARG.h
+++ OpenARG/OpenARG.h
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 
+/// @brief GNU-Style Command Parser
 class OpenARG
 {
 public:
@@ -16,15 +17,47 @@ public:
 
 public:
 	OpenARG(int argc, char** argv);
+
 	OpenARG(std::string const& args);
+
 	~OpenARG();
 
+	/// @brief Get item at index
+	/// @param index 
+	/// @return 
 	item_t const& item(size_t index) const;
+
+	/// @brief Get all items
+	/// @return 
 	std::vector<item_t> const& items() const;
+
+	/// @brief Check swich existed
+	/// @param name 
+	/// @return 
 	bool exist(std::string const& name) const;
+
+	/// @brief Check swich existed & get index
+	/// @param name 
+	/// @param result 
+	/// @return 
 	bool index(std::string const& name, size_t& result) const;
+
+	/// @brief Check swich existed & get indices
+	/// @param name 
+	/// @param result 
+	/// @return 
 	bool index(std::string const& name, std::vector<size_t>& result) const;
+
+	/// @brief Get swich value if existed, otherwise return default value
+	/// @param name 
+	/// @param value default value
+	/// @return 
 	std::string value(std::string const& name, std::string const& value = std::string()) const;
+
+	/// @brief Get swich values if existed, otherwise return default value
+	/// @param name 
+	/// @param value default value
+	/// @return 
 	std::vector<std::string> value(std::string const& name, std::vector<std::string> const& value = {}) const;
 
 protected:
